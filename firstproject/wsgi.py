68531d39c75add51248459791da4175701622d1c
kaushik dobariya <kaus.patel777@gmail.com>
2021-08-26 19:41:47 +0530

    Updated files

--- firstproject/wsgi.py
+++ firstproject/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+application = WhiteNoise(application, root='/static')
+application.add_files('/static', prefix='more-files/')
